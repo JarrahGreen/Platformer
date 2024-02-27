ab8b67e96a3018fe5443fc303ad7f0878663d5be
JarrahGreen <135961440+JarrahGreen@users.noreply.github.com>
2024-02-27 18:20:13 +0000

    Basis of the code

--- main.cpp
+++ main.cpp
@@ -1,16 +1,62 @@
 #include <SFML/Graphics.hpp>
 
+
+float x = 10;
+float y = 10;
+float target_x;
+float target_y;
+
+void changeX() {
+    if (target_x > x) {
+        x++ ;
+    } else if (target_x < x) {
+        x-- ;
+    } else {
+        ;
+    }
+}
+
+void changeY() {
+    if (target_y > y) {
+        y++ ;
+    }
+    if (target_y < y) {
+        y-- ;
+    }
+    else {
+        ;
+    }
+}
+
+
+
+
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(640, 480), "SFML Application");
+
+    sf::RenderWindow window(sf::VideoMode(1920, 1080), "SFML Application");
     sf::CircleShape shape;
-    shape.setRadius(40.f);
-    shape.setPosition(100.f, 100.f);
+
+
+
+    shape.setRadius(20.f);
+
     shape.setFillColor(sf::Color::Cyan);
+
+
 
     while (window.isOpen())
     {
-        sf::Event event;
+        sf::Event event{};
+        target_x = sf::Mouse::getPosition(window).x - 20;
+        target_y = sf::Mouse::getPosition(window).y - 20;
+
+        changeX();
+        changeY();
+
+
+
+        shape.setPosition(x, y);
 
         while (window.pollEvent(event))
         {
