bd06e59671b92593025670717d9a515c85e75018
JarrahGreen <135961440+JarrahGreen@users.noreply.github.com>
2024-03-09 11:25:09 +0000

    Adding a sprite-sheet

--- main.cpp
+++ main.cpp
@@ -1,39 +1,59 @@
 #include <SFML/Graphics.hpp>
 
-
 int main()
 {
-    printf("test");
-    printf("test");
-    //Initialisation
+    // Initialisation
+    enum {Up, Left, Right};
+
+    sf::Vector2i  source(1, Up);
+
+    int imageSize = 32;
+
     unsigned int windowWidth = 800.0f;
     unsigned int windowHeight = 600.0f;
 
-    float floorHeight = 100.0f;
-    float floorWidth = 800.0f;
-
     sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight), "Dylan's Game");
+    window.setKeyRepeatEnabled(false);
 
-    sf::RectangleShape floor(sf::Vector2f(floorWidth,floorHeight));
-    floor.setFillColor(sf::Color(100,10,10));
-    floor.setPosition(0.0f,float(windowHeight)-floorHeight);
+    sf::Texture texture;
+    sf::Sprite playerImage;
+
+    if (!texture.loadFromFile("C:/Users/dylan/OneDrive - West Herts College/C Lion/HelloSFML/test.png")) {
+
+    }
+
+    playerImage.setTexture(texture);
+
+    // Set position of the playerImage sprite to the center of the window
+    playerImage.setPosition(windowWidth / 2.0f, windowHeight / 2.0f);
 
     while (window.isOpen()) {
-
         // Update
-        sf::Event event{};
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed) {
-                window.close();
+        sf::Event Event{};
+        while (window.pollEvent(Event)) {
+            switch (Event.type) {
+                case sf::Event::Closed:
+                    window.close();
+                    break;
+                case sf::Event::KeyPressed:
+                    if (Event.key.code == sf::Keyboard::Up)
+                        source.y = Up;
+                    else if(Event.key.code == sf::Keyboard::Left)
+                        source.y = Left;
+                    else if(Event.key.code == sf::Keyboard::Right)
+                        source.y = Right;
             }
         }
 
+        source.x++;
+        if (source.x * imageSize >= texture.getSize().x)
+            source.x = 0;
+
         // Draw
-        window.clear(sf::Color::Black);
-        window.draw(floor);
+        playerImage.setTextureRect(sf::IntRect(source.x * imageSize, source.y * imageSize, imageSize, imageSize));
+        window.draw(playerImage);
         window.display();
+        window.clear(sf::Color::Blue);
     }
-
-
     return 0;
 }
