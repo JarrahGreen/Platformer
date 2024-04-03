e94477bc60a84d0b7a36e455d9112dcfc1e20af7
JarrahGreen <135961440+JarrahGreen@users.noreply.github.com>
2024-04-03 16:28:50 +0100

    Add spritesheet

--- main.cpp
+++ main.cpp
@@ -3,11 +3,14 @@
 int main()
 {
     // Initialisation
-    enum {Up, Left, Right};
+    enum {RightUp, RightRun, RightIdle, LeftUp, LeftRun, LeftIdle};
 
-    sf::Vector2i  source(1, Up);
+    sf::Vector2i  source(1, RightUp);
+
+    float frameCounter = 0, switchFrame = 100, frameSpeed = 500;
 
     int imageSize = 32;
+    int idle;
 
     unsigned int windowWidth = 800.0f;
     unsigned int windowHeight = 600.0f;
@@ -17,15 +20,16 @@ int main()
 
     sf::Texture texture;
     sf::Sprite playerImage;
+    sf::Clock clock;
 
-    if (!texture.loadFromFile("C:/Users/dylan/OneDrive - West Herts College/C Lion/HelloSFML/test.png")) {
+    if (!texture.loadFromFile("C:/Users/dylan/OneDrive - West Herts College/C Lion/HelloSFML/Spritesheet.png")) {
 
     }
 
     playerImage.setTexture(texture);
 
     // Set position of the playerImage sprite to the center of the window
-    playerImage.setPosition(windowWidth / 2.0f, windowHeight / 2.0f);
+    playerImage.setPosition(0, 568);
 
     while (window.isOpen()) {
         // Update
@@ -35,25 +39,38 @@ int main()
                 case sf::Event::Closed:
                     window.close();
                     break;
-                case sf::Event::KeyPressed:
-                    if (Event.key.code == sf::Keyboard::Up)
-                        source.y = Up;
-                    else if(Event.key.code == sf::Keyboard::Left)
-                        source.y = Left;
-                    else if(Event.key.code == sf::Keyboard::Right)
-                        source.y = Right;
             }
         }
 
-        source.x++;
-        if (source.x * imageSize >= texture.getSize().x)
-            source.x = 0;
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+            source.y = RightRun;
+            playerImage.move(0.1,0);
+            idle = 0;
+        }
+        else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+            source.y = LeftRun;
+            playerImage.move(-0.1,0);
+            idle = 1;
+        }
+        else
+            if (idle == 1)
+                source.y = LeftIdle;
+            else if (idle == 0)
+                source.y = RightIdle;
+
+        frameCounter += frameSpeed * clock.restart().asSeconds();
+        if (frameCounter >= switchFrame) {
+            frameCounter = 0;
+            source.x++;
+            if (source.x * imageSize >= texture.getSize().x)
+                source.x = 0;
+        }
 
         // Draw
         playerImage.setTextureRect(sf::IntRect(source.x * imageSize, source.y * imageSize, imageSize, imageSize));
         window.draw(playerImage);
         window.display();
-        window.clear(sf::Color::Blue);
+        window.clear(sf::Color::Black);
     }
     return 0;
 }
