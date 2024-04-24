06ba76734a330efed58682cd5547e362ae999bcb
JarrahGreen <135961440+JarrahGreen@users.noreply.github.com>
2024-04-24 14:09:26 +0100

    Background

---
+++ cmake-build-debug/CMakeFiles/HelloSFML.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HelloSFML.dir/main.cpp.obj"
+  "CMakeFiles/HelloSFML.dir/main.cpp.obj.d"
+  "HelloSFML.exe"
+  "HelloSFML.exe.manifest"
+  "HelloSFML.pdb"
+  "libHelloSFML.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HelloSFML.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
